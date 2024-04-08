1e429d084cfafea11ce15facc021a24395152eb4
linzhiji <linzhiji@gmail.com>
2024-04-08 20:21:20 +0800

    fix bug

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import sys,os
-
 import torch
+
+WORKDIR = "/data/work/GPT-SoVITS/"
 
 # 推理用的指定模型
 sovits_path = ""
@@ -10,12 +11,12 @@ is_half = True if is_half_str.lower() == 'true' else False
 is_share_str = os.environ.get("is_share","False")
 is_share= True if is_share_str.lower() == 'true' else False
 
-cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
-bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
+cnhubert_path = f"{WORKDIR}GPT_SoVITS/pretrained_models/chinese-hubert-base"
+bert_path = f"{WORKDIR}GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
 
 # 本地训练的模型
-pretrained_sovits_path = "GPT_SoVITS/pretrained_models/s2G488k.pth"
-pretrained_gpt_path = "GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
+pretrained_sovits_path = f"{WORKDIR}GPT_SoVITS/pretrained_models/s2G488k.pth"
+pretrained_gpt_path = f"{WORKDIR}GPT_SoVITS/pretrained_models/s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt"
 
 exp_root = "logs"
 python_exec = sys.executable or "python"
